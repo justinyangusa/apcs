e173fdbd5a244451a2d158d8ea8965f292bcf5cd
Justin Yang <justinyangusa@gmail.com>
2013-09-15 23:19:30 -0700

    Cleaned files

--- SummerHomework/7.scm
+++
@@ -1,8 +0,0 @@
-; Justin Yang
-; APCS Summer Homework #7
-
-(define (filter pred lst)
-	(cond ((null? lst) '())
-		((pred (car lst))
-		 (cons (car lst) (filter pred (cdr lst))))
-		(else (filter pred (cdr lst)))))
